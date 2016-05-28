63ff9831bd26e89d4a93e35d66c167a2a2e18015
novikov-alexander <alex-novikov@pochta.ru>
2016-05-28 20:16:03 +0300

    Temp variables for transferring

--- heatequation/main.cpp
+++ heatequation/main.cpp
@@ -4,25 +4,32 @@
 int rank, size;
 
 class Grid {
-	double step;
+	double xstep, ystep;
 public:
 	double *data;
 	double height, width;
 	int x, y;
 
-	double getStep(){
-		return step;
+	double getXStep(){
+		return xstep;
+	}
+
+	double getYStep(){
+		return xstep;
 	}
 
 	Grid(int x, int y, double height, double width){
+		int portionx = (x / (size + 1));
+		int i1x = (rank == 0) ? 0 : x - portionx * (size - rank);
+		int i2x = x - portionx * (size - 1 - rank);
+		x = i2x - i1x;
+
 		this->height = height;
 		this->width = width;
-		this->step = height / y;
+		this->xstep = height / x;
+		this->ystep = height / y;
 		this->x = x;
 		this->y = y;
-		if (this->step != width / x){
-			throw "Not uniform grid.";
-		}
 		data = new double[x*y];
 	}
 };
@@ -44,95 +51,139 @@ public:
 		return 0;
 	}
 	*/
+	inline void tma_seq(double gamma, double alpha, double beta, int s_size, double *f, int step)
+	{
+		double *p = new double[s_size], *q = new double[s_size];
+		double *y = new double[s_size];
+		p[0] = 0;
+		q[0] = f[0];
+		for (int i = 0; i<s_size-2; ++i)
+		{
+			p[i + 1] = beta / (alpha - gamma * p[i]);
+			q[i + 1] = (gamma * q[i] + f[i*step]) / (alpha - gamma * p[i]);
+		}
+		y[s_size-1] = f[s_size-1];
+		for (int i = s_size - 2; i >= 0; --i)
+			y[i] = p[i] * y[i + 1] + q[i];
+	}
+
 	inline void tma(double gamma, double alpha, double beta, int s_size, double *b){
 		MPI_Status status;
-		int portion = (s_size / (size + 1));
-		int i1 = (rank == 0) ? 0 : s_size - portion * (size - rank);
-		int i2 = s_size - portion * (size - 1 - rank);
-		double *u = new double[i2 - i1 + 1];
-		double *l = new double[i2 - i1 + 1];
-		double *S = new double[i2 - i1 + 1];
-		double *t = new double[i2 - i1 + 1];
-		double *T = new double[i2 - i1 + 1];
-		double *y = new double[i2 - i1 + 1];
-		double *x = new double[i2 - i1 + 1];
-
+		double ty,tx;
+		double *u = new double[s_size];
+		double *l = new double[s_size];
+		double *S = new double[s_size];
+		double *t = new double[s_size];
+		double *T = new double[s_size];
+		double *y = new double[s_size];
+		double *x = new double[s_size];
+		
 		if (rank == 0){
 			u[0] = alpha;
 			t[0] = 0;
 			S[1] = alpha;
 			T[1] = -beta*gamma;
-			for (int i = 2; i <= i2; i++){
+
+			for (int i = 2; i < s_size; i++){
 				S[i] = alpha - beta*gamma/ S[i - 1];
 				T[i] = (alpha*T[i - 1] - beta*gamma*t[i - 2]) / S[i - 1];
 				t[i - 1] = T[i - 1] / S[i - 1];
 				u[i] = T[i] - S[i] * t[i - 1];
 			}
-			u[1] = T[1] /*- S[1] * t[0]*/;
-			for (int i = 1; i <= i2; i++){
+			u[1] = T[1];
+			for (int i = 1; i < s_size; i++){
 				u[i] = S[i] + u[i] / (u[0] + t[i - 1]);
 			}
-			MPI_Send(&u[i2 - 1], 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
-			for (int i = 1; i <= i2; i++){
+			MPI_Send(&u[s_size - 1], 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+			for (int i = 1; i < s_size; i++){
 				l[i] = gamma / u[i];
 			}
 			y[0] = b[0];
-
 		}
 		else {
+			double tu;
 			t[0] = 0;
-			S[1] = alpha;
-			T[1] = -beta*gamma;
-			for (int i = 2; i <= i2 - i1; i++){
+			S[0] = alpha;
+			T[0] = -beta*gamma;
+			for (int i = 1; i < s_size; i++){
 				S[i] = alpha - beta*gamma / S[i - 1];
 				T[i] = (alpha*T[i - 1] - beta*gamma*t[i - 2]) / S[i - 1];
-				t[i - 1] = T[i - 1] / S[i - 1];
-				u[i] = T[i] - S[i] * t[i - 1];
+				t[i] = T[i - 1] / S[i - 1];
+				u[i] = T[i] - S[i] * t[i];
 			}
-			u[1] = T[1];
-			MPI_Recv(&u[0], 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &status);
-			for (int i = 1; i <= i2 - i1; i++){
-				u[i] = S[i] + u[i] / (u[0] + t[i - 1]);
+			u[0] = T[0];
+			
+			MPI_Recv(&tu, 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &status);
+			for (int i = 0; i < s_size; i++){
+				u[i] = S[i] + u[i] / (tu + t[i]);
 
 			}
 			if (rank < size - 1){
-				MPI_Send(&u[i2 - i1], 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD);
+				MPI_Send(&u[s_size - 1], 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD);
 			}
-			for (int i = 1; i <= i2 - i1; i++){
+			for (int i = 0; i < s_size; i++){
 				l[i] = gamma / u[i];
 			}
-			MPI_Recv(&y[0], 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &status);
+			MPI_Recv(&ty, 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &status);
+			y[0] = b[0] - l[0] * ty;
 		}
 
-		for (int i = 0; i < i2 - i1; i++){
+		for (int i = 0; i < s_size - 1; i++){
 			y[i + 1] = b[i + 1] - l[i + 1] * y[i];
 		}
+		
 		if (rank < size - 1){
-			MPI_Send(&y[i2 - i1], 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD);
-			MPI_Recv(&x[i2 - i1], 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &status);
+			MPI_Send(&y[s_size - 1], 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD);
+			MPI_Recv(&tx, 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &status);
+			x[s_size - 1] = (y[s_size - 1] - tx * beta) / u[s_size - 1];
 		} else {
-			x[i2 - i1] = y[i2 - i1] / u[i2 - i1];
+			x[s_size - 1] = y[s_size - 1] / u[s_size - 1];
 		}
-		for (int i = i2 - i1 - 1; i >= 0; --i){
+		for (int i = s_size - 2; i >= 0; --i){
 			x[i] = (y[i] - x[i + 1] * beta) / u[i];
-			std::cout << x[i] << std::endl;
 		}
 		if (rank!=0)
 			MPI_Send(&x[0], 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD);
+		delete[] u;
+		delete[] l;
+		delete[] S;
+		delete[] t;
+		delete[] T;
+		delete[] x;
+		delete[] y;
+	}
+
+	inline double f(int i, int j){
+		return 0;
 	}
 
 	inline void solve(Grid *src, Grid *dst){
-		double step = src->getStep();
+		double stepx = src->getXStep();
 		int maxi = src->x - 1, maxj = src->y - 1;
 		double *srcData = src->data, *dstData = dst->data;
-		double hh = step*step;
-		double *b = new double[maxi-2];
-		for (int i = 0; i < maxi - 2; i++){
-			b[i] = 0;
+		double hh = stepx*stepx;
+		double *b = new double[maxi * maxj];
+		double **bb = new double*[maxj];
+		for (int j = 0; j < maxi*maxj; j++){
+			b[j] = 0;
 		}
-		b[0] = 10.0/tau;
-		b[maxi - 3] = 10.0/tau;
-		tma(1.0 / hh, 1.0 / tau - 2.0 / hh, 1.0 / hh, maxi-2, b);
+		for (int j = 0; j < maxj; j++){
+			bb[j] = &b[j*maxi];
+		}
+		for (int i = 0; i < maxi; ++i){
+			for (int j = 0; j < maxj; ++j){
+				b[i] = -srcData[i*maxi + j] / (tau / 2) - f(i, j) / 2;
+			}
+		}
+		
+		for (int j = 1; j < maxj - 1; j++){
+			tma(1.0 / hh, 1.0 / (tau/2) - 2.0 / hh, 1.0 / hh, maxi - 1, bb[j]);
+			MPI_Barrier(MPI_COMM_WORLD);
+		}
+
+		for (int i = 1; i < maxi - 1; i++){
+			tma_seq(1.0 / hh, 1.0 / (tau/2) - 2.0 / hh, 1.0 / hh, maxj - 2, b, maxi - 2);
+		}
 	}
 	
 	Solver(double tau){
@@ -148,9 +199,15 @@ int main(int argc, char *argv[]){
 
 	int x = 1000, y = 1000;
 	double h = 1, w = 1;
-	Solver solver(0.5);
+	
+	x = x - 2;
+	y = y - 2;
+
+	
 	Grid *myGrid = new Grid(x, y, h, w);
 	Grid *nextOne = new Grid(x, y, h, w);
+
+	Solver solver(0.5);
 	solver.solve(myGrid, nextOne);
 	MPI_Finalize();
 	return 0;
